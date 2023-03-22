4354545fd44d2262c310acb50544b1522de5363f
Moussafarota04 <moussafarota50@yahoo.fr>
2023-03-22 15:22:16 -0400

    terraform code

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+region        = "us-west-1"
+ami           = "ami-06604eb73be76c003"
+instance-type = "t2.nano"
---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable  "region" {
+    type    = string
+    default = "us-east-1"
+}
+
+variable  "ami" {
+    type    = string
+    default = "ami-005f9685cb30f234b"
+}
+
+variable  "instance-type" {
+    type    = string
+    default = "t2.micro"
+}
